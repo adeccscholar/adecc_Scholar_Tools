4570e93d1778b1fc0d405d14120ac88bedb1be45
Volker Hillmann <vhillmann@ad.adecc.de>
2023-08-24 00:58:01 +0200

    Erweiterung von MyError.h

--- adecc_Tools/MyError.h
+++ adecc_Tools/MyError.h
@@ -3,6 +3,15 @@
 #include <string>
 #include <utility>
 
-enum class EMyErrorType : int { noError = 0, Userbreak, InputError, FileError, unknown = 999 };
+/// enum with error types for MyErrorInfo
+enum class EMyErrorType : int { noError = 0,   ///< no error 
+                                Userbreak,     ///< user canceled the function
+                                InputError,    ///< input error of the user
+                                FileError,     ///< error in a file operation 
+                                DatabaseError, ///< error in the context of the database
+                                RuntimeError,  ///< unspecific runtime error
+                                unknown = 999  ///< unknown or unexpected error 
+                               };
 
+/// type for unexpected returns of functions
 using MyErrorInfo = std::pair<EMyErrorType, std::string>;
