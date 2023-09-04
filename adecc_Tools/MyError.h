93b16e686af963e3efef9edb29ac377afb134b9a
Volker Hillmann <vhillmann@ad.adecc.de>
2023-09-04 19:53:03 +0200

    Erweiterung für die Fehler

--- adecc_Tools/MyError.h
+++ adecc_Tools/MyError.h
@@ -5,6 +5,7 @@
 
 /// enum with error types for MyErrorInfo
 enum class EMyErrorType : int { noError = 0,   ///< no error 
+                                NotSupported,  ///< the operation isn't supported
                                 Userbreak,     ///< user canceled the function
                                 InputError,    ///< input error of the user
                                 FileError,     ///< error in a file operation 
